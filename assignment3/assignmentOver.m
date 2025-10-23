47c52cdf21fa474670a90546cb68c9660959f13e
noelloch <lochern22@students.ecu.edu>
2025-10-22 22:34:04 -0400

    Create assignmentOver.m

---
+++ assignment3/assignmentOver.m
@@ -0,0 +1,6 @@
+function assignmentOver
+% This is a function that displays the text "Thank goodness this assignment
+% is over!" when called upon. 
+
+disp("Thank goodness this assignment is over!")
+end 
